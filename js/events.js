2e7bdd0179f2beca9fc174e1c4fd0ae616a3d123
Amanda Baydar <amandabaydar@gmail.com>
2020-09-29 16:14:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,7 @@
 function getIt(){$('p').on("click", function(){alert("Hey!")})}
 function frameIt(){$('img').on("load", function(){$('img').addClass("tasty")})}
 function pressIt(){$('#typing').on('keydown', function(press){if (press.which === 71){alert('G was pressed')}})}
+function submitIt(){
 
 $(document).ready(function(){
 
