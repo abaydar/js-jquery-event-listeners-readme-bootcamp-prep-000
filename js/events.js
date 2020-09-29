6af02c0411ab17d218da4ae6d8d0f6ffde1f6422
Amanda Baydar <amandabaydar@gmail.com>
2020-09-29 16:11:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 //define functions here
 function getIt(){$('p').on("click", function(){alert("Hey!")})}
 function frameIt(){$('img').on("load", function(){$('img').addClass("tasty")})}
-function pressIt(){$(document).on('keydown',function(key){if (key.which === 103){alert('G was pressed')}})}
+function pressIt(){$(document).on('keydown', function(key){if (key.which === 103){alert('G was pressed')}})}
 
 $(document).ready(function(){
 
